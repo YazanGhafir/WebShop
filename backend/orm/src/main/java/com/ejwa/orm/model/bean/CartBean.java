1050c61c2a2282e02dd1a3885e1e579fd122889e
Madeleine Lexén <lexen@student.chalmers.se>
2020-03-19 17:35:11 +0100

    Creates order without customer id

--- backend/orm/src/main/java/com/ejwa/orm/model/bean/CartBean.java
+++ backend/orm/src/main/java/com/ejwa/orm/model/bean/CartBean.java
@@ -6,8 +6,11 @@
 package com.ejwa.orm.model.bean;
 
 import com.ejwa.orm.model.dao.ClothingItemDAO;
+import com.ejwa.orm.model.dao.CustomerDAO;
+import com.ejwa.orm.model.dao.CustomerOrderClothingItemDAO;
 import com.ejwa.orm.model.dao.CustomerOrderDAO;
 import com.ejwa.orm.model.entity.ClothingItem;
+import com.ejwa.orm.model.entity.Customer;
 import com.ejwa.orm.model.entity.CustomerOrder;
 import com.ejwa.orm.model.entity.CustomerOrderClothingItem;
 import java.io.Serializable;
@@ -18,8 +21,6 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
-import javax.enterprise.context.SessionScoped;
-import javax.inject.Named;
 import lombok.Data;
 import lombok.Getter;
 
@@ -30,22 +31,28 @@ import lombok.Getter;
 @Data
 @Singleton
 public class CartBean implements Serializable {
-
+    
     @EJB
     private ClothingItemDAO clothingItemDAO;
-
+    
     @EJB
     private CustomerOrderDAO customerOrderDAO;
-
+    
+    @EJB
+    private CustomerOrderClothingItemDAO customerOrderClothingItemDAO;
+    
+    @EJB
+    private CustomerDAO customerDAO;
+    
     @Getter
     private List<CartItem> items;
-
+    
     private String customerInfo;
     
     private String customerEmail;
     
     private boolean inloggningsstatus = false;
-
+    
     public void addItem(Long id, String size) {
         ClothingItem ci = clothingItemDAO.findClothingItemMatchingID(id);
         boolean found = false;
@@ -66,7 +73,7 @@ public class CartBean implements Serializable {
         }
         items = newList;
     }
-
+    
     public void updateQuantity(long id, int quantity, String size) {
         items.forEach(i -> {
             if (i.getItem().getId() == (id) && i.getSize().equals(size)) {
@@ -74,7 +81,7 @@ public class CartBean implements Serializable {
             }
         });
     }
-
+    
     public void removeItem(Long id, String size) {
         List<CartItem> newList = new ArrayList<CartItem>();
         items.forEach(i -> {
@@ -84,7 +91,7 @@ public class CartBean implements Serializable {
         });
         items = newList;
     }
-
+    
     @PostConstruct
     public void init() {
         this.items = new ArrayList<CartItem>();
@@ -92,34 +99,40 @@ public class CartBean implements Serializable {
         clothingItemDAO.create(ci);
         items.add(new CartItem(ci, "L", 1));
     }
-
+    
     public String createOrder() {
         LocalDateTime test_date = LocalDateTime.of(2014, Month.SEPTEMBER, 11, 16, 15, 15);;
         CustomerOrder order = new CustomerOrder(test_date);
         List<CustomerOrderClothingItem> clothesList = new ArrayList<CustomerOrderClothingItem>();
+        Customer customer = customerDAO.findCustomerMatchingEmail(customerEmail);
+        //order.setCustomer(customer);
+        customerOrderDAO.create(order);
         items.forEach(i -> {
             CustomerOrderClothingItem c = new CustomerOrderClothingItem();
-            c.setClothingItem(clothingItemDAO.find(i.getItem().getId()));
+            c.setClothingItem(i.getItem());
             c.setQuantity(i.getQuantity());
             c.setSize(i.getSize());
+            c.setCustomerOrder(order);
+            customerOrderClothingItemDAO.create(c);
             clothesList.add(c);
         });
-        order.setClothesList(clothesList);
-        customerOrderDAO.create(order);
+        CustomerOrder order1 = customerOrderDAO.find(order.getCustomerorder_id());
+        order1.setClothesList(clothesList);
+        customerOrderDAO.edit(order1);
         return "Order Created";
     }
-
+    
     public void addCustomerInfoAfterLogin(String customerInfo) {
         this.customerInfo = customerInfo;
         this.inloggningsstatus = true;
     }
-
+    
     public void removeCustomerInfoAfterLogout() {
         this.customerInfo = "A Great Person :D";
     }
     
-    public List<CustomerOrder> getHistory(){
+    public List<CustomerOrder> getHistory() {
         return this.customerOrderDAO.findCustomerOrdersMatchingCustomer(customerEmail);
-    }    
-
+    }
+    
 }
